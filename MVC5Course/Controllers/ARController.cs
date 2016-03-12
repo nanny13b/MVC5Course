d092c4cff00b544d19e2ef8ac2392a2decbcec46
nanny13b <nanny13b@hotmail.co>
2016-03-12 13:53:25 +0800

    26 練習不同的 ActionResult 用法 ( View, PartialView, Content, File )

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ARController : Controller
+    {
+        // GET: AR
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AR
+        public ActionResult Index2()
+        {
+            return PartialView("Index");
+        }
+
+        public ContentResult ContentTest()
+        {
+            return Content("<script>alert('redirecting.....');</script>", "application/javascript", Encoding.UTF8);
+
+        }
+
+        public FileResult FileTest()
+        {
+            return File(Server.MapPath("~/Content/yoga.jpg"), "image/jpeg");
+        }
+    }
+}
