0c086ad30611923665117a40be1747b20866730f
nanny13b <nanny13b@hotmail.co>
2016-03-13 11:36:16 +0800

    35 練習透過 HandlerError 自訂不同例外狀況下的自訂錯誤畫面

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -37,6 +38,22 @@ namespace MVC5Course.Controllers
 
         public ActionResult Test()
         {
+            return View();
+        }
+
+        [HandleError(ExceptionType = typeof(ArgumentException), View ="ErrorArgument")]
+        [HandleError(ExceptionType = typeof(SqlException), View = "ErrorSqlException")]
+        public ActionResult ErrorTest(string e)
+        {
+            if (e == "1")
+            {
+                throw new Exception("Error 1");
+            }
+
+            if (e == "2")
+            {
+                throw new ArgumentException("Error 2");
+            }
             return View();
         }
     }
