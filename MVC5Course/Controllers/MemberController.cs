f8b077dcc5f13f44acb4c3af9f47540c3fac38d3
nanny13b <nanny13b@hotmail.co>
2016-03-13 11:16:50 +0800

    實作表單驗證 (FormsAuthentication) **尚未完成**

---
+++ MVC5Course/Controllers/MemberController.cs
@@ -0,0 +1,51 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC5Course.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+        //因為還沒登入，所以要設定可以匿名
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Action(LoginViewModel login)
+        {            
+            if (CheckLogin(login.Email, login.Password))
+            {
+                //這句話的意思?
+                //false > true 就是記住我的功能
+                FormsAuthentication.RedirectFromLoginPage(login.Email, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("Password", "認證錯誤，請重新輸入");
+
+            return View();
+        }
+
+        private bool CheckLogin(string email, string password)
+        {
+            //修改從資料庫檢查
+            return (email == "nanny13b@hotmail.com" && password == "123");
+            throw new NotImplementedException();
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return View();
+        }
+    }
+}
